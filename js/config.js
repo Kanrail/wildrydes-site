0d43906c8d9e032e16107f52eb8e634c1238734f
Daniel Bowen <generalkan@hotmail.com>
2021-01-28 08:59:03 -0600

    updating config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_GzUL7WcS0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1akpp1c4q83ins44gmgt3ts4ai', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
